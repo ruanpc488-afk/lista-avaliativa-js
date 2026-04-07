aeca54e7e1e4446253a959ac04b40d1e21039ada
ruanpc488-afk <ruanpc488@gmail.com>
2026-04-07 14:08:24 -0300

    Create questao2.js

---
+++ questao2.js
@@ -0,0 +1,17 @@
+let num = 10; // Número para teste
+
+// Teste de positivo ou negativo
+if (num > 0) {
+    console.log("O número é positivo");
+} else if (num < 0) {
+    console.log("O número é negativo");
+} else {
+    console.log("O número é zero");
+}
+
+// Teste de par ou ímpar
+if (num % 2 == 0) {
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
+}
