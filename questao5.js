c3234854bf86b15f4ade23203388d4e2f50f1233
ruanpc488-afk <ruanpc488@gmail.com>
2026-04-07 14:09:27 -0300

    Create questao5.js

---
+++ questao5.js
@@ -0,0 +1,15 @@
+let n1 = 8;
+let n2 = 6;
+let media = (n1 + n2) / 2;
+
+console.log("Sua média foi: " + media);
+
+if (media == 10) {
+    console.log("Desempenho Excelente");
+} else if (media >= 7) {
+    console.log("Aprovado");
+} else if (media >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
